6e8483d7ada006f82588dd9615d69f1cb6a26853
Kirby Browning <kirby.browning718@gmail.com>
2020-07-07 07:47:55 -0400

    letter typing and space bar functionality added

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-var keys = document.getElementsByClassName('keyboard-key');
-
-
 var screen = document.getElementById('screen');
-screen.innerHTML = '';
 
+var keys = document.getElementsByClassName('keyboard-key');
 
 for(var i=0; i< keys.length; i++) {
 
 
-
   keys[i].addEventListener("click", function (e) {
+    if(e.target.classList.contains("letter")) {
+
     var characters = e.target.innerHTML;
 
 var typedCharacters = document.createElement('span');
 typedCharacters.innerHTML = characters;
+ {
 screen.appendChild(typedCharacters);
+}
+}
 
+if(e.target.classList.contains("space")) {
+  var space = document.createTextNode(" ");
 
+screen.appendChild(space);
+}
 
 });
+
 }
