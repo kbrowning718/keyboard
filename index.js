5497344bea7fc6f3266a86cf61485316d10b32c7
Kirby Browning <kirbybrowning@Kirbys-Air.fios-router.home>
2021-01-31 17:55:38 -0500

    updated JS for enter functionality

--- index.js
+++ index.js
@@ -3,34 +3,30 @@ var keys = document.getElementsByClassName('keyboard-key');
 
 
 
-// starting array as empty; screen.innerHTML is the "screen"
-
 let screenArray = [];
 screen.innerHTML = screenArray;
 
-//click event for the letter keys; check to see if the class contains "letter"
+
 
 for(var i=0; i<keys.length; i++) {
   keys[i].addEventListener("click", function(e) {
     if(e.target.classList.contains('letter')) {
 
-      // variable letters is the innerHTML of whatever was clicked
 
-      let letters = e.target.innerHTML;
+
+      var letters = e.target.innerHTML;
       console.log(letters);
 
-      //typedLetters is the new span element created in the screen.innerHTML
 
       var typedLetters = document.createElement('span');
       typedLetters.innerHTML = letters;
-
       screenArray.push(letters);
       screen.appendChild(typedLetters);
   }
 
 
 if(e.target.classList.contains('number')) {
-  let numberLabel = e.target.children[1].innerHTML;
+  var numberLabel = e.target.children[1].innerHTML;
   console.log(numberLabel);
 
   var typedNumbers = document.createElement('span');
@@ -38,6 +34,18 @@ if(e.target.classList.contains('number')) {
 
   screenArray.push(numberLabel);
   screen.appendChild(typedNumbers);
+}
+
+
+if(e.target.classList.contains('enter')) {
+    var enter = document.createElement('p');
+
+  var newLine = [];
+
+newLine.push(enter.innerHTML);
+
+screen.appendChild(enter);
+
 }
 
 
