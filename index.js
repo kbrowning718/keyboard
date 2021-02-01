f056124fcf9bfb19ac85305c4ba982f48f74a7b2
Kirby Browning <kirbybrowning@Kirbys-Air.fios-router.home>
2021-01-31 20:19:49 -0500

    prettier

--- index.js
+++ index.js
@@ -1,65 +1,55 @@
-const screen = document.getElementById('screen');
-const keys = document.getElementsByClassName('keyboard-key');
+const screen = document.getElementById("screen");
+const keys = document.getElementsByClassName("keyboard-key");
 
 let screenArray = [];
 
 const renderParagraphs = () => {
-  screen.innerHTML = ""
+  screen.innerHTML = "";
   return screenArray.map((s) => {
-  const paragraph = document.createElement('p');
-  paragraph.innerText = s.join('');
-  return screen.appendChild(paragraph);
+    const paragraph = document.createElement("p");
+    paragraph.innerText = s.join("");
+    return screen.appendChild(paragraph);
+  });
+};
 
-})
-}
-
-
-
-for(let i=0; i<keys.length; i++) {
-  keys[i].addEventListener("click", function(e) {
-    if(e.target.classList.contains('letter')) {
-      if(!screenArray.length > 0) {
+for (let i = 0; i < keys.length; i++) {
+  keys[i].addEventListener("click", function (e) {
+    if (e.target.classList.contains("letter")) {
+      if (!screenArray.length > 0) {
         screenArray.push([]);
       }
-const letter = e.target.innerText;
-screenArray[screenArray.length-1].push(letter);
-renderParagraphs()
-  }
+      const letter = e.target.innerText;
+      screenArray[screenArray.length - 1].push(letter);
+      renderParagraphs();
+    }
 
+    if (e.target.classList.contains("number")) {
+      var numberLabel = e.target.children[1].innerHTML;
+      console.log(numberLabel);
 
-if(e.target.classList.contains('number')) {
-  var numberLabel = e.target.children[1].innerHTML;
-  console.log(numberLabel);
+      var typedNumbers = document.createElement("span");
+      typedNumbers.innerHTML = numberLabel;
 
-  var typedNumbers = document.createElement('span');
-  typedNumbers.innerHTML = numberLabel;
+      screenArray.push(numberLabel);
+      screen.appendChild(typedNumbers);
+    }
 
-  screenArray.push(numberLabel);
-  screen.appendChild(typedNumbers);
-}
+    if (e.target.classList.contains("enter")) {
+      screenArray.push([]);
+    }
 
+    if (e.target.classList.contains("delete")) {
+      const currentParagraph = screenArray[screenArray.length - 1];
+      if (currentParagraph.length <= 0) {
+        screenArray.pop();
+      } else {
+        currentParagraph.pop();
+      }
+      renderParagraphs();
+    }
 
-if(e.target.classList.contains('enter')) {
-    screenArray.push([]);
-
-}
-
-
-if(e.target.classList.contains('delete')) {
-  const currentParagraph = screenArray[screenArray.length-1];
-  if(currentParagraph.length <= 0) {
-    screenArray.pop();
-  } else {
-    currentParagraph.pop()
-  }
-  renderParagraphs();
-}
-
-if(e.target.classList.contains('space')) {
-  screenArray.push(" ");
-}
-
-
-});
-
+    if (e.target.classList.contains("space")) {
+      screenArray.push("");
+    }
+  });
 }
